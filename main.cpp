f0a8de7c62cd8597105796d1c8fe0859f93c20bd
홍길동 <@naver.com>
2023-12-08 14:28:43 +0900

    코드 정리 및 상태창 함수

--- main.cpp
+++ main.cpp
@@ -6,6 +6,37 @@
 #include <string.h>
 
 //*************************** 클래스 정의 ***************************
+class Ral {
+public:
+    int money; //소지금
+    int clean; //청결도
+    int tired; //피로도
+    int hunger; //배고픔 
+    int willing; //의지
+    sf::Vector2f size = { 40, 40 };
+    sf::Texture texture; //텍스쳐
+    sf::RectangleShape player; //몸
+
+    Ral() {
+        money = 10000;
+        clean = 30;
+        tired = 0;
+        hunger = 50;
+        willing = 20;
+        if (!texture.loadFromFile(".\\lib\\frog.png")) {/*error...*/ } //텍스처 불러오기
+        player.setTextureRect(sf::IntRect(0, 0, size.x, size.y)); //텍스쳐 모양 선언
+        player.setSize(sf::Vector2f( 80, 120 )); //플레이어 크기
+        player.setTexture(&texture);
+        player.setPosition(sf::Vector2f(500, 250));
+    }
+    int getMoney() { return money; }
+    int getClean() { return clean; }
+    int getHunger() { return hunger; }
+    int getWilling() { return willing; }
+    sf::RectangleShape getPlayer() { return player; }
+    sf::Vector2f getSize() { return size; }
+};
+
 class Stage {
 public:
     virtual void onEnter() = 0; //스테이지에 입장
@@ -29,24 +60,22 @@ public:
 };
 
 class Level1 : public Stage {
-private:
+protected:
     const float gridSize = 40.f; //격자 크기
     const float movementSpeed = 400.f;
     const unsigned window_width = 1280;
     const unsigned window_height = 720;
-    int money;
 
+    Ral p1;
+    sf::Text status;
+    sf::Vector2f textPos = { 1035, 10 };
     sf::Font font;
-    sf::Text moneyText;
-    sf::Text moneyValue;
     sf::Vector2f center{ 640,360 }; //화면 중앙 좌표
     sf::Vector2f velocity;
     sf::RectangleShape save;
     sf::RectangleShape load;
     sf::RectangleShape maru; //마루
-    sf::RectangleShape player;
     sf::RectangleShape room; //방 배경 객체
-    sf::Texture texturePlayer; //텍스처 객체
     sf::Texture textureMaru; //마루 객체
     sf::Texture textureSave; //save 버튼 텍스쳐
     sf::Texture textureLoad; //load 버튼 텍스쳐
@@ -58,6 +87,7 @@ public:
     void onEnter() override;
     void update(sf::RenderWindow& window, float dt) override;
     void handleinput(sf::RenderWindow& window, sf::Event& event) override;
+    void printStatus(sf::RenderWindow& window, Ral player);
     void onExit() override;
 };
 
@@ -71,6 +101,7 @@ public:
     void changeStage(const std::string& stageName);
     void run();
 };
+
 
 // ************************* onEnter(), onExit()함수 *****************************
 void Menu::onEnter() {
@@ -102,7 +133,7 @@ void Level1::update(sf::RenderWindow& window, float dt) {
     window.clear(sf::Color::Black);
 
     for (auto& line : lines) {
-        sf::FloatRect playerBounds = player.getGlobalBounds();
+        sf::FloatRect playerBounds = p1.player.getGlobalBounds();
         sf::FloatRect lineBounds = line.getGlobalBounds();
         sf::FloatRect nextPos = playerBounds;
         nextPos.left += velocity.x * dt;
@@ -114,24 +145,23 @@ void Level1::update(sf::RenderWindow& window, float dt) {
         }
     }
 
-    player.move(velocity * dt);
+    p1.player.move(velocity * dt);
 
     // Player가 화면 바깥으로 나가는 것을 막는 코드
-    if (player.getPosition().x < 0) player.setPosition(0, player.getPosition().y);
-    if (player.getPosition().y < 0) player.setPosition(player.getPosition().x, 0);
-    if (player.getPosition().x > window_width - gridSize) player.setPosition(window_width - gridSize, player.getPosition().y);
-    if (player.getPosition().y > window_height - gridSize) player.setPosition(player.getPosition().x, window_height - gridSize);
+    if (p1.player.getPosition().x < 0) p1.player.setPosition(0, p1.player.getPosition().y);
+    if (p1.player.getPosition().y < 0) p1.player.setPosition(p1.player.getPosition().x, 0);
+    if (p1.player.getPosition().x > window_width - gridSize) p1.player.setPosition(window_width - gridSize, p1.player.getPosition().y);
+    if (p1.player.getPosition().y > window_height - gridSize) p1.player.setPosition(p1.player.getPosition().x, window_height - gridSize);
 
     for (auto& line : lines) {
         window.draw(line);
     }
-    window.draw(moneyText);
-    window.draw(moneyValue);
+    printStatus(window, p1);
     window.draw(save);
     window.draw(load);
-    window.draw(room);
-    window.draw(maru);
-    window.draw(player);
+    //window.draw(room);
+    //window.draw(maru);
+    window.draw(p1.player);
 }
 
 // ************************** handleinput **************************
@@ -170,21 +200,17 @@ void Level1::handleinput(sf::RenderWindow& window, sf::Event& event) {
     }
 
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up) {
-        money += 100;
-        moneyValue.setString(std::to_string(money));
+        p1.money += 100;
 
     }
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right) {
-        money += 1;
-        moneyValue.setString(std::to_string(money));
+        p1.money += 1;
     }
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down) {
-        money -= 100;
-        moneyValue.setString(std::to_string(money));
+        p1.money -= 100;
     }
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left) {
-        money -= 1;
-        moneyValue.setString(std::to_string(money));
+        p1.money -= 1;
     }
     if (event.type == sf::Event::KeyReleased) {
         if (event.key.code == sf::Keyboard::W ||
@@ -204,7 +230,7 @@ void Level1::handleinput(sf::RenderWindow& window, sf::Event& event) {
                 std::cout << "save game data" << std::endl;
                 std::ofstream file("game_save.txt");
                 if (file.is_open()) {
-                    file << money << "\n";
+                    file << p1.money << "\n";
                     file.close();
                 }
                 else {
@@ -219,8 +245,8 @@ void Level1::handleinput(sf::RenderWindow& window, sf::Event& event) {
                 std::cout << "load game data" << std::endl;
                 std::ifstream file("game_save.txt");
                 if (file.is_open()) {
-                    file >> money;
-                    moneyValue.setString(std::to_string(money));
+                    file >> p1.money;
+                    //moneyValue.setString(std::to_string(p1.money));  스테이터스 출력할 코드 넣기
                     file.close();
                 }
                 else {
@@ -236,6 +262,7 @@ void Level1::handleinput(sf::RenderWindow& window, sf::Event& event) {
 }
 
 // ************************* 생성자 함수 ***********************
+
 Menu::Menu() {
     if (!font.loadFromFile(".\\lib\\Maplestory Light.ttf")) { //폰트 업로드
         std::cout << "Could not load font" << std::endl;
@@ -256,32 +283,18 @@ Menu::Menu() {
 }
 
 Level1::Level1() {
-    //플레이어
-    if (!texturePlayer.loadFromFile(".\\lib\\frog.png")) {/*error...*/ } //텍스처 불러오기
-    player.setTextureRect(sf::IntRect(0, 0, gridSize, gridSize)); //텍스쳐 모양 선언
-    player.setSize(sf::Vector2f(gridSize, gridSize)); //플레이어 크기
-    player.setTexture(&texturePlayer);
-    player.setPosition(240, 240);
-
-    //money 텍스트
     if (!font.loadFromFile(".\\lib\\Maplestory Light.ttf")) { //폰트 업로드
         std::cout << "Could not load font" << std::endl;
     }
-    moneyText.setOrigin(0, 25);
-    moneyText.setFont(font); //폰트 적용
-    moneyText.setString("Money: "); //출력 문자열
-    moneyText.setCharacterSize(20); //문자 크기
-    moneyText.setStyle(sf::Text::Bold); //폰트 스타일
-    moneyText.setPosition(1020, 40); //글자 위치
 
-    //money 값
-    money = 0;
-    moneyValue.setOrigin(0, 25);
-    moneyValue.setFont(font); //폰트 적용
-    moneyValue.setString(std::to_string(money)); //출력 문자열
-    moneyValue.setCharacterSize(20); //문자 크기
-    moneyValue.setStyle(sf::Text::Bold); //폰트 스타일
-    moneyValue.setPosition(1100, 40); //글자 위치
+    ////money 값
+    //money = 0;
+    //moneyValue.setOrigin(0, 25);
+    //moneyValue.setFont(font); //폰트 적용
+    //moneyValue.setString(std::to_string(money)); //출력 문자열
+    //moneyValue.setCharacterSize(20); //문자 크기
+    //moneyValue.setStyle(sf::Text::Bold); //폰트 스타일
+    //moneyValue.setPosition(1100, 40); //글자 위치
 
     //save 버튼
     if (!textureSave.loadFromFile(".\\lib\\save.png")) {/*error...*/ } //텍스처 불러오기
@@ -327,18 +340,18 @@ Level1::Level1() {
 }
 
 Game::Game() {
-    
-        window.create(sf::VideoMode(1280, 720), "Baeksu Simulator");
-        sf::Image icon; //아이콘 객체
-        if (!icon.loadFromFile(".\\lib\\ral.png"))  // 아이콘 경로 설정
-        {  /* 이미지 파일 로드 실패 시 처리*/
-        }
-        window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr()); //아이콘 적용
-        stages["menu"] = std::make_unique<Menu>();
-        stages["level1"] = std::make_unique<Level1>();
-        currentStage = stages["menu"].get();
-        currentStage->onEnter();
-    
+
+    window.create(sf::VideoMode(1280, 720), "Baeksu Simulator");
+    sf::Image icon; //아이콘 객체
+    if (!icon.loadFromFile(".\\lib\\ral.png"))  // 아이콘 경로 설정
+    {  /* 이미지 파일 로드 실패 시 처리*/
+    }
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr()); //아이콘 적용
+    stages["menu"] = std::make_unique<Menu>();
+    stages["level1"] = std::make_unique<Level1>();
+    currentStage = stages["menu"].get();
+    currentStage->onEnter();
+
 }
 
 // ********************** Game() 메소드  ************************
@@ -375,6 +388,38 @@ void Game::run() {
         currentStage->update(window, dt);
         window.display();
     }
+}
+
+//************************* Level1 메소드 *********************************
+void Level1::printStatus(sf::RenderWindow& window, Ral player) {
+    status.setFont(font); //폰트 적용
+    status.setStyle(sf::Text::Bold); //폰트 스타일
+    status.setString(L"== 상태창 =="); //출력 문자열
+    status.setCharacterSize(40); //문자 크기
+    status.setPosition(textPos); //글자 위치
+    window.draw(status);
+
+    status.setCharacterSize(25);
+    status.setPosition(sf::Vector2f(textPos.x, textPos.y+80));
+    status.setString(L"소지금: " + std::to_wstring(p1.money));
+    window.draw(status);
+
+    status.setPosition(sf::Vector2f(textPos.x, textPos.y + 160));
+    status.setString(L"피로도: " + std::to_wstring(p1.tired));
+    window.draw(status);
+
+    status.setPosition(sf::Vector2f(textPos.x, textPos.y + 240));
+    status.setString(L"청결도: " + std::to_wstring(p1.clean));
+    window.draw(status);
+
+    status.setPosition(sf::Vector2f(textPos.x, textPos.y + 320));
+    status.setString(L"배고픔: " + std::to_wstring(p1.hunger));
+    window.draw(status);
+
+    status.setPosition(sf::Vector2f(textPos.x, textPos.y + 400));
+    status.setString(L"의지: " + std::to_wstring(p1.willing));
+    window.draw(status);
+
 }
 
 
